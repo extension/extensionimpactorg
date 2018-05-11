ecdf70cf4580441a456e09251575e4509c1745bd
Jason Young <jay@outfielding.net>
2018-05-11 09:02:44 -0400

    remove dev host

--- config/deploy/dev.rb
+++
@@ -1,9 +0,0 @@
-set :deploy_to, "/services/impact/"
-if(branch = ENV['BRANCH'])
-  set :branch, branch
-else
-  set :branch, 'master'
-end
-set :vhost, 'dev.extensionimpact.org'
-set :deploy_server, 'dev-impact.awsi.extension.org'
-server deploy_server, :app, :web, :db, :primary => true
